c7acc1d06c53d94d2eb3cd0bd969858cb9e37c1f
claudiama <claudiama0916@gmail.com>
2021-09-05 16:32:19 +0800

    completed lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,10 +1,20 @@
-/** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+/**
+ * Class that determines whether or not a year is a leap year.
+ *
+ * @author YOUR NAME HERE
  */
 public class LeapYear {
 
-    /** Calls isLeapYear to print correct statement.
-     *  @param  year to be analyzed
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else return year % 4 == 0 && year % 100 != 0;
+    }
+
+    /**
+     * Calls isLeapYear to print correct statement.
+     *
+     * @param year to be analyzed
      */
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
@@ -14,7 +24,9 @@ public class LeapYear {
         }
     }
 
-    /** Must be provided an integer as a command line argument ARGS. */
+    /**
+     * Must be provided an integer as a command line argument ARGS.
+     */
     public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("Please enter command line arguments.");
